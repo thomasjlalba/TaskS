2988567469e8b20e66b151c72b2d18d18b298d7c
thomasjlalba <e0773507@u.nus.edu>
2023-02-17 17:33:24 +0800

    add sth

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -19,10 +19,11 @@ public class Main {
         printData(tasksData);
         System.out.println();
         System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
+        printDeadlinesUsingStream(tasksData);
 
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
-
+        ArrayList<Task> FilteredList = filterTaskListUsingStreams(tasksData, "11");
+        System.out.println("\n Filtered list of tasks");
+        
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -47,5 +48,19 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+    
+    public static void printDeadlineUsingStream(ArrayList<Task> tasks) {
+        tasks.stream()
+                .filter(t-> instanceof Deadline)
+                .sorted((a, b) -> a.getDescription().compareToIgnoreCase(b.getDescription()))
+                .forEach(System.out::println);
+    }
+    
+    public static ArrayList<Task> filterTaskListUsingStreams(ArrayList<task> tasks, String filterString) {
+        ArrayList<task> filteredList = (ArrayList<task>) tasks.stream()
+                .filter(t -> t.getDescription().contains(filterString))
+                .collect(toList());
+        return filteredList;
     }
 }
